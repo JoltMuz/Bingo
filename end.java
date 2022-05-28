529e9da1430aacbda840169ed5d1da38ba2f124b
JoltTheBolt <93381290+JoltMuz@users.noreply.github.com>
2022-05-28 19:17:18 +0500

    Update end.java

--- end.java
+++ end.java
@@ -15,24 +15,31 @@ public class end implements CommandExecutor {
     @Override
     public boolean onCommand(CommandSender commandSender, Command command, String s, String[] args)
     {
-        Bukkit.broadcastMessage(bingo.signature + ChatColor.YELLOW + ChatColor.BOLD.toString() + "The Bingo Event has ended!");
-        List<String> keys = points.points.entrySet().stream().sorted(Map.Entry.<String, Integer>comparingByValue().reversed()).limit(3).map(Map.Entry::getKey).collect(Collectors.toList());
-        if (keys.size() > 0)
-        {
-            Bukkit.broadcastMessage(bingo.signature + ChatColor.AQUA + "♕ #1 》" + keys.get(0));
-        }
-        if (keys.size() > 1)
-        {
-            Bukkit.broadcastMessage(bingo.signature + ChatColor.YELLOW + "♖ #2 》" + keys.get(1));
-        }
-        if (keys.size() > 2)
-        {
-            Bukkit.broadcastMessage(bingo.signature + ChatColor.WHITE + "♘ #3 》" + keys.get(2));
-        }
-        if (round.RoundisRunning)
-        {
-            round.round.cancel();
-        }
+       if (commandSender.isOp())
+    	{
+    		Bukkit.broadcastMessage(bingo.signature + ChatColor.YELLOW + ChatColor.BOLD.toString() + "The Bingo Event has ended!");
+            List<String> keys = points.points.entrySet().stream().sorted(Map.Entry.<String, Integer>comparingByValue().reversed()).limit(3).map(Map.Entry::getKey).collect(Collectors.toList());
+            if (keys.size() > 0)
+            {
+                Bukkit.broadcastMessage(bingo.signature + ChatColor.AQUA + "♕ #1 》" + keys.get(0));
+            }
+            if (keys.size() > 1)
+            {
+                Bukkit.broadcastMessage(bingo.signature + ChatColor.YELLOW + "♖ #2 》" + keys.get(1));
+            }
+            if (keys.size() > 2)
+            {
+                Bukkit.broadcastMessage(bingo.signature + ChatColor.WHITE + "♘ #3 》" + keys.get(2));
+            }
+            if (round.RoundisRunning)
+            {
+                round.round.cancel();
+            }
+    	}
+    	else
+    	{
+    		commandSender.sendMessage(bingo.signature + ChatColor.RED + "You must be operator to use this command!");
+    	}
 
         return true;
     }
